1bd8f57726d638b30921f022220d7c2cdd23fa2c
J-E Castagnede <j.e.castagnede@gmail.com>
2022-11-03 17:43:36 +0100

    improve admin

--- project/cumulus/admin.py
+++ project/cumulus/admin.py
@@ -5,4 +5,6 @@ from project.cumulus.models import Data
 
 @admin.register(Data)
 class DataAdmin(admin.ModelAdmin):
-    list_display = ('real_datetime',)
+    list_display = (
+        'real_datetime', 'temperature', 'humidity', 'windspeed', 'wind_direction', 'barometer', 'rain_per_hour'
+    )
