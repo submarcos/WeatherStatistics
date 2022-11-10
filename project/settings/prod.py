bf9fa3ea9b9034baa3ff0a5443021f12554a596d
J-E Castagnede <j.e.castagnede@gmail.com>
2022-11-10 16:19:47 +0100

    enable sentry

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -1,4 +1,9 @@
 import os
+import sentry_sdk
+
+from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.celery import CeleryIntegration
+from sentry_sdk.integrations.redis import RedisIntegration
 
 from . import *  # NOQA
 
@@ -11,3 +16,21 @@ CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = [
     f"https://{os.getenv('DOMAIN')}",
 ]
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    integrations=[
+        DjangoIntegration(),
+        CeleryIntegration(),
+        RedisIntegration()
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
